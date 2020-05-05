4b73bc9f13727195271c7a076a6b33843a41090e
NoWay <gosustyle87+gitlab@gmail.com>
2020-05-06 00:28:22 +0200

    Initial commit.

--- predict.py
+++ predict.py
@@ -60,12 +60,6 @@ def load_from_checkpoint(path):
         
     return model_transfer
 
-transform = transforms.Compose([transforms.Resize(256),
-                                      transforms.CenterCrop(224),
-                                     transforms.ToTensor(),
-                                     transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                              std=[0.229, 0.224, 0.225])])
-
 def process_image(image):
     ''' Scales, crops, and normalizes a PIL image for a PyTorch model,
         returns an Numpy array
